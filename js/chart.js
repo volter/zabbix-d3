1a6b86f4b53435a342e5224c859d99307f8dd1ad
Corey Shaw <coshaw@adobe.com>
2014-01-09 11:35:14 -0700

    Initial attempts to draw an SVG path

--- js/chart.js
+++ js/chart.js
@@ -1,76 +1,78 @@
-jQuery(document).ready(function(){
-	//d3.select("#graph_cont1").append("svg").attr("width", 50).attr("height", 50).append("circle").attr("cx", 25).attr("cy", 25).attr("r", 25).style("fill", "purple");
-		
-		// define dimensions of graph
-		
-		//var m = [80, 80, 80, 80]; // margins
-		var graphData = [];
-		//var w = graphJson["fullSizeX"]; // width
-		//var h = graphJson["fullSizeY"]; // height
-		for(var i in graphJson)
-			result.push([i, graphData[i]]);
-		
-		result.forEach(function(itemArray){
-			console.log(itemArray);
-		});
-//		/*graphData.
-//		graphData.forEach(function(itemArray){
-//			console.log(itemArray);
-//		});*/
-	
-		// create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
-		var data = [3, 6, 2, 7, 5, 2, 0, 3, 8, 9, 2, 5, 9, 3, 6, 3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 2, 7];
-/*
-		// X scale will fit all values from data[] within pixels 0-w
-		var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
-		// Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
-		var y = d3.scale.linear().domain([0, 10]).range([h, 0]);
-			// automatically determining max range can work something like this
-			// var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
+jQuery(document).ready(function() {
+    //d3.select("#graph_cont1").append("svg").attr("width", 50).attr("height", 50).append("circle").attr("cx", 25).attr("cy", 25).attr("r", 25).style("fill", "purple");
 
-		// create a line function that can convert data[] into x and y points
-		var line = d3.svg.line()
-			// assign the X function to plot our line as we wish
-			.x(function(d,i) { 
-				// verbose logging to show what's actually being done
-				console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
-				// return the X coordinate where we want to plot this datapoint
-				return x(i); 
-			})
-			.y(function(d) { 
-				// verbose logging to show what's actually being done
-				console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
-				// return the Y coordinate where we want to plot this datapoint
-				return y(d); 
-			})
+    // define dimensions of graph
 
-			// Add an SVG element with the desired dimensions and margin.
-			var graph = d3.select("#graph_cont1").append("svg:svg")
-			      .attr("width", w + m[1] + m[3])
-			      .attr("height", h + m[0] + m[2])
-					.append("svg:g")
-			      .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-
-			// create yAxis
-			var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
-			
-			// Add the x-axis.
-			graph.append("svg:g")
-			      .style("class", "x axis")
-			      .attr("transform", "translate(0," + h + ")")
-			      .call(xAxis);
+    var m = [80, 80, 80, 80]; // margins
+    var result = [];
+    //var w = graphData["fullSizeX"]; // width
+    //var h = graphData["fullSizeY"]; // height
+    //for(var i in graphData)
+    //	result.push(graphData[i]);
 
 
-			// create left yAxis
-			var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
-			
-			// Add the y-axis to the left
-			graph.append("svg:g")
-			      .attr("class", "y axis")
-			      .attr("transform", "translate(-25,0)")
-			      .call(yAxisLeft);
-			
-  			// Add the line by appending an svg:path element with the data line we created above
-			// do this AFTER the axes above so that the line is above the tick-lines
-  			graph.append("svg:path").attr("d", line(data));*/
+    var lineData = [];
+    for (var i in graphData['data']) {
+        for (x in graphData['data'][i][0]['avg']) {
+            var xvalue = graphData['data'][i][0]['avg'][x];
+            var yvalue = graphData['data'][i][0]['clock'][x];
+            lineData.push({'x': xvalue, 'y': yvalue});
+        }
+//        console.log(i);
+        //data = graphData['data'][i][0]['avg'];
+        // var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
+        //var y = d3.scale.linear().domain([d3.min(data), d3.max(data)]).range([h, 0]);
+
+    }
+    console.log(lineData);
+
+    /*result.forEach(function(itemArray){
+     console.log(itemArray);
+     });
+     */
+    // create a line function that can convert data[] into x and y points
+
+    /*  var line = d3.svg.line()
+     .x(function(d) {
+     return d.x;
+     })
+     .y(function(d) {
+     return d.y;
+     })
+     .interpolate("linear");
+     
+     // Add an SVG element with the desired dimensions and margin.
+     var graph = d3.select("#graph_cont1").append("svg:svg")
+     .attr("width", 800)
+     .attr("height", 600);
+     
+     var lineGraph = graph.append("path")
+     .attr("d", line(data))
+     .attr("stroke", "blue")
+     .attr("stroke-width", 2)
+     .attr("fill", "none");
+     console.log("Done"); 
+     */
+
+    var lineFunction = d3.svg.line()
+            .x(function(d) {
+                return d.x;
+            })
+            .y(function(d) {
+                return d.y;
+            })
+            .interpolate("linear");
+
+//The SVG Container
+    var svgContainer = d3.select("#graph_cont1").append("svg")
+            .attr("width", 400)
+            .attr("height", 400);
+
+//The line SVG Path we draw
+    var lineGraph = svgContainer.append("path")
+            .attr("d", lineFunction(lineData))
+            .attr("stroke", "blue")
+            .attr("stroke-width", 2)
+            .attr("fill", "none");
+    console.log("Done");
 });
