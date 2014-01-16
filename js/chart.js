e16b5ce6df60b9b6df8e71c1b2f3d74384fedd44
Corey Shaw <coshaw@adobe.com>
2014-01-15 20:04:38 -0700

    Fixed reversed x/y values

--- js/chart.js
+++ js/chart.js
@@ -4,7 +4,6 @@ jQuery(document).ready(function() {
     // define dimensions of graph
 
     var m = [80, 80, 80, 80]; // margins
-    var result = [];
     //var w = graphData["fullSizeX"]; // width
     //var h = graphData["fullSizeY"]; // height
     //for(var i in graphData)
@@ -12,18 +11,30 @@ jQuery(document).ready(function() {
 
 
     var lineData = [];
+    var max_x, max_y, min_y, min_x;
     for (var i in graphData['data']) {
+        min_y = d3.min(graphData['data'][i][0]['avg']);
+        max_y = d3.max(graphData['data'][i][0]['avg']);
+        min_x = d3.min(graphData['data'][i][0]['clock']);
+        max_x = d3.max(graphData['data'][i][0]['clock']);
+
         for (x in graphData['data'][i][0]['avg']) {
-            var xvalue = graphData['data'][i][0]['avg'][x];
-            var yvalue = graphData['data'][i][0]['clock'][x];
+            var xvalue = graphData['data'][i][0]['clock'][x];
+            var yvalue = graphData['data'][i][0]['avg'][x];
             lineData.push({'x': xvalue, 'y': yvalue});
         }
+
 //        console.log(i);
         //data = graphData['data'][i][0]['avg'];
         // var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
         //var y = d3.scale.linear().domain([d3.min(data), d3.max(data)]).range([h, 0]);
 
     }
+
+    var linearScale = d3.scale.linear()
+            .domain([min_x, max_x])
+            .range([0, 100]);
+
     console.log(lineData);
 
     /*result.forEach(function(itemArray){
@@ -71,7 +82,7 @@ jQuery(document).ready(function() {
 //The line SVG Path we draw
     var lineGraph = svgContainer.append("path")
             .attr("d", lineFunction(lineData))
-            .attr("stroke", "blue")
+            .attr("stroke", "black")
             .attr("stroke-width", 2)
             .attr("fill", "none");
     console.log("Done");
